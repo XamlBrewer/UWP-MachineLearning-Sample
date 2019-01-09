735fff6530a5c3f2bb84f849f75f05d3f34fa119
Diederik Krols <diederik_krols@hotmail.com>
2019-01-09 21:56:17 +0100

    Clustering page: move logic to viewmodel

---
+++ XamlBrewer.Uwp.MachineLearningSample/ViewModels/ClusteringPageViewModel.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.ML.Core.Data;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Runtime.KMeans;
+using Mvvm;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XamlBrewer.Uwp.MachineLearningSample.Models;
+
+namespace XamlBrewer.Uwp.MachineLearningSample.ViewModels
+{
+    internal class ClusteringPageViewModel : ViewModelBase
+    {
+        private LocalEnvironment _mlContext = new LocalEnvironment(seed: null); // v0.6;
+
+        public EstimatorChain<ClusteringPredictionTransformer<KMeansPredictor>> Pipeline { get; private set; }
+
+        public ITransformer Model { get; private set; }
+
+        public ClusteringPageViewModel()
+        { }
+
+        public async Task<IDataView> Load(string trainingDataPath)
+        {
+            return await Task.Run(() =>
+            {
+                var reader = new TextLoader(_mlContext,
+                                            new TextLoader.Arguments()
+                                            {
+                                                Separator = ",",
+                                                HasHeader = true,
+                                                Column = new[]
+                                                    {
+                                                    new TextLoader.Column("CustomerId", DataKind.I4, 0),
+                                                    new TextLoader.Column("Gender", DataKind.Text, 1),
+                                                    new TextLoader.Column("Age", DataKind.I4, 2),
+                                                    new TextLoader.Column("AnnualIncome", DataKind.R4, 3),
+                                                    new TextLoader.Column("SpendingScore", DataKind.R4, 4),
+                                                    }
+                                            });
+
+                var file = _mlContext.OpenInputFile(trainingDataPath);
+                var src = new FileHandleSource(file);
+                return reader.Read(src);
+            });
+        }
+
+        public async Task Build()
+        {
+            await Task.Run(() =>
+            {
+                Pipeline = new ConcatEstimator(_mlContext, "Features", "AnnualIncome", "SpendingScore")
+                    .Append(new KMeansPlusPlusTrainer(_mlContext, "Features", clustersCount: 5));
+            });
+        }
+
+        public async Task Train(IDataView trainingDataView)
+        {
+            await Task.Run(() =>
+            {
+                Model = Pipeline.Fit(trainingDataView);
+            });
+        }
+
+        public async Task<IEnumerable<ClusteringPrediction>> Predict(IDataView dataView)
+        {
+            return await Task.Run(() =>
+            {
+                var result = Model.Transform(dataView).AsEnumerable<ClusteringPrediction>(_mlContext, false); ;
+                return result;
+            });
+        }
+
+        public async Task<ClusteringPrediction> Predict(ClusteringData clusteringData)
+        {
+            return await Task.Run(() =>
+            {
+                var predictionFunc = Model.MakePredictionFunction<ClusteringData, ClusteringPrediction>(_mlContext);
+                return predictionFunc.Predict(clusteringData);
+            });
+        }
+    }
+}
--- XamlBrewer.Uwp.MachineLearningSample/Views/ClusteringPage.xaml.cs
+++ XamlBrewer.Uwp.MachineLearningSample/Views/ClusteringPage.xaml.cs
@@ -1,23 +1,16 @@
-﻿using Microsoft.ML.Core.Data;
-using Microsoft.ML.Runtime.Api;
-using Microsoft.ML.Runtime.Data;
-using Microsoft.ML.Runtime.KMeans;
-using Mvvm.Services;
+﻿using Mvvm.Services;
 using OxyPlot;
 using OxyPlot.Annotations;
 using OxyPlot.Axes;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using Windows.UI.Xaml.Controls;
 using XamlBrewer.Uwp.MachineLearningSample.Models;
+using XamlBrewer.Uwp.MachineLearningSample.ViewModels;
 
 namespace XamlBrewer.Uwp.MachineLearningSample
 {
     public sealed partial class ClusteringPage : Page
     {
-        private ITransformer _model;
-        private LocalEnvironment _mlContext;
         private List<OxyColor> _colors = new List<OxyColor>
                 {
                     OxyColors.Black,
@@ -32,8 +25,12 @@ namespace XamlBrewer.Uwp.MachineLearningSample
         public ClusteringPage()
         {
             this.InitializeComponent();
+            this.DataContext = new ClusteringPageViewModel();
+
             Loaded += Page_Loaded;
         }
+
+        private ClusteringPageViewModel ViewModel => DataContext as ClusteringPageViewModel;
 
         private async void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
@@ -42,52 +39,49 @@ namespace XamlBrewer.Uwp.MachineLearningSample
             TrainingBox.IsChecked = false;
             CalculatingBox.IsChecked = false;
             PlottingBox.IsChecked = false;
+            PrepareDiagram();
 
-            //Preparing the files.
+            // Preparing the files.
             DatasetBox.IsChecked = true;
             var trainingDataPath = await MlDotNet.FilePath(@"ms-appx:///Data/Mall_Customers.csv");
 
-            //Create the MLContext
+            // Read training data.
+            var trainingDataView = await ViewModel.Load(trainingDataPath);
+
+            // Create the model.
             SettingUpBox.IsChecked = true;
-            _mlContext = new LocalEnvironment(seed: null); // v0.6
-            var reader = new TextLoader(_mlContext,
-                                        new TextLoader.Arguments()
-                                        {
-                                            Separator = ",",
-                                            HasHeader = true,
-                                            Column = new[]
-                                                {
-                                                    new TextLoader.Column("CustomerId", DataKind.I4, 0),
-                                                    new TextLoader.Column("Gender", DataKind.Text, 1),
-                                                    new TextLoader.Column("Age", DataKind.I4, 2),
-                                                    new TextLoader.Column("AnnualIncome", DataKind.R4, 3),
-                                                    new TextLoader.Column("SpendingScore", DataKind.R4, 4),
-                                                }
-                                        });
+            await ViewModel.Build();
 
-            // Transform your data and add a learner
-            var pipeline = new ConcatEstimator(_mlContext, "Features", "AnnualIncome", "SpendingScore")
-                   .Append(new KMeansPlusPlusTrainer(_mlContext, "Features", clustersCount: 5));
-
-            //Load training data
-            //var file = mlContext.OpenInputFile(@"ms-appx:///Assets/Mall_Customers.csv");
-            var file = _mlContext.OpenInputFile(trainingDataPath);
-            var src = new FileHandleSource(file);
-            var trainingDataView = reader.Read(src);
-
-            // Create and train the model      
+            // Train the model.    
             TrainingBox.IsChecked = true;
-            _model = pipeline.Fit(trainingDataView);
+            await ViewModel.Train(trainingDataView);
 
             // Could save the model here.
             // ...
 
-            // Produce the results.
+            // Run the model on a set of data.
             CalculatingBox.IsChecked = true;
-            var result = _model.Transform(trainingDataView);
+            var predictions = await ViewModel.Predict(trainingDataView);
 
             // Draw the results.
             PlottingBox.IsChecked = true;
+            foreach (var prediction in predictions)
+            {
+                Diagram.Model.Annotations.Add(
+                    new PointAnnotation
+                    {
+                        Shape = MarkerType.Circle,
+                        X = prediction.SpendingScore,
+                        Y = prediction.AnnualIncome,
+                        Fill = _colors[(int)prediction.PredictedCluster]
+                    });
+            }
+
+            Diagram.InvalidatePlot();
+        }
+
+        private void PrepareDiagram()
+        {
             var foreground = OxyColors.LightSteelBlue;
             var plotModel = new PlotModel
             {
@@ -116,30 +110,13 @@ namespace XamlBrewer.Uwp.MachineLearningSample
             plotModel.Axes.Add(linearAxisY);
             Diagram.Model = plotModel;
             linearAxisY.Reset();
-            var outp = result.AsEnumerable<ClusteringPrediction>(_mlContext, false);
-            var enu = outp.GetEnumerator();
-            while (enu.MoveNext())
-            {
-                var prediction = enu.Current;
-                var annotation = new PointAnnotation
-                {
-                    Shape = MarkerType.Circle,
-                    X = prediction.SpendingScore,
-                    Y = prediction.AnnualIncome,
-                    Fill = _colors[(int)prediction.PredictedCluster]
-                };
-                plotModel.Annotations.Add(annotation);
-            }
-
-            Diagram.InvalidatePlot();
         }
 
-        private void Calculate_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private async void Calculate_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             int.TryParse(AnnualIncomeInput.Text, out int annualIncome);
             int.TryParse(SpendingScoreInput.Text, out int spendingScore);
-            var predictionFunc = _model.MakePredictionFunction<ClusteringData, ClusteringPrediction>(_mlContext);
-            var output = predictionFunc.Predict(new ClusteringData { AnnualIncome = annualIncome, SpendingScore = spendingScore });
+            var output = await ViewModel.Predict(new ClusteringData { AnnualIncome = annualIncome, SpendingScore = spendingScore });
             var annotation = new PointAnnotation { Shape = MarkerType.Diamond, X = output.SpendingScore, Y = output.AnnualIncome, Fill = _colors[(int)output.PredictedCluster], TextColor = OxyColors.LightSteelBlue, Text = "Here" };
             Diagram.Model.Annotations.Add(annotation);
             Diagram.InvalidatePlot();
